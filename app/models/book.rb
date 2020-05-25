0a5d0f2545eb15d9ab7cf85b317f5c95c5dd31cc
ryotaro matsumoto <ryotaro@ryotaro-MacBook-Pro.local>
2020-05-25 18:19:51 +0900

    re-submit

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,6 @@ class Book < ApplicationRecord
 	belongs_to :user, optional: true
 
 	validates :title, presence: true
-	validates :body, presence: true, length: {maximum: 50}
+	validates :body, presence: true, length: {maximum: 200}
 
 end
